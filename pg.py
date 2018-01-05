ad5e9c65328b20177093b9e90604c0fa2d6bd1d1
Joshua Smith (CW) <joshuabuckleysmith@gmail.com>
2018-01-05 12:58:51 -0800

    '2nd'

---
+++ pg.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+b=0
+root = tk.Tk()
+
+#class tkbuttons:
+#    class checkwin:
+#        target = tk.StringVar()
+#        def p():
+            
+#            print(tkbuttons.checkwin.target)
+class tkbuttons:   
+    def __init__(self, z):
+        self.one = z
+    def pfd():
+        target = tk.StringVar()
+        cw = tkbuttons
+            #def t():
+            #    print('hi')
+
+print(tkbuttons("a"))
+print(tkbuttons.pfd())
+#tkbuttons.checkwin.p()
